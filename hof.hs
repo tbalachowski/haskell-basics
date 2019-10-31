2b945deae92effc1775d8cb608247c445f954a6a
Tomasz Balachowski <t.balachowski@gmail.com>
2019-10-31 19:17:07 +0100

    higher order functions

--- hof.hs
+++ hof.hs
@@ -42,3 +42,47 @@ largestDivisible :: (Integral a) => a
 largestDivisible = head (filter p [100000..])
     where p x = x `mod` 3829 == 0
 
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+    | even n = n :chain (n `div` 2)
+    | odd n = n :chain (n * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+sum' :: (Num a) => [a] -> a
+sum' = foldl (+) 0
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+map'' :: (a -> b) -> [a] -> [b]
+map'' f xs = foldr (\x acc -> f x : acc) [] xs
+
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldl1 (\acc x -> if x > acc then x else acc)
+
+reverse' :: [a] -> [a]
+reverse' = foldl (flip (:)) []
+
+product' :: (Num a) => [a] -> a
+product' = foldl1 (*)
+
+filter'' :: (a -> Bool) -> [a] -> [a]
+filter'' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+head' :: [a] -> a
+head' = foldl1 (\x _ -> x)
+
+last' :: [a] -> a
+last' = foldr1 (\_ acc -> acc)
+
+sqrtSums :: Int
+sqrtSums = length (takeWhile (<1000) ((scanl1 (+) (map sqrt [1..])))) + 1
+
+oddSquareSum :: Integer
+oddSquareSum =
+    let oddSquares = filter odd . map (^2) $ [1..]
+        belowLimit = takeWhile (<10000) oddSquares
+    in sum belowLimit
