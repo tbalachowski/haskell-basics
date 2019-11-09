f5dd69d9a06dc36c5e5260eff9ffd1549d16e3ce
Tomasz Balachowski <t.balachowski@gmail.com>
2019-11-09 11:18:49 +0100

    TrafficLight as instance of Eq typeclass

--- typeclasses.hs
+++ typeclasses.hs
@@ -77,3 +77,45 @@ treeElem x (Node a left right)
     | x == a = True
     | x < a  = treeElem x left
     | x > a  = treeElem x right
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+    Red == Red = True
+    Yellow == Yellow = True
+    Green == Green = True
+    _ == _ = False
+
+instance Show TrafficLight where
+    show Red = "Red light"
+    show Yellow = "Yellow light"
+    show Green = "Green light"
+
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _  = True
+
+instance YesNo Bool where
+    yesno = id
+    
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing  = False
+
+instance YesNo (Tree a) where
+    yesno EmptyTree = False
+    yesno _ = True
+
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _ = True
+
+yesnoIf :: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult = if yesno yesnoVal then yesResult else noResult
